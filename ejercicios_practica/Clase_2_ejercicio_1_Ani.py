22874c358aca5f1d860b5d4cd2e073daa02d9b57
anahi-jc18 <84754142+anahi-jc18@users.noreply.github.com>
2022-09-04 18:33:24 -0300

    Add files via upload

---
+++ ejercicios_practica/Clase_2_ejercicio_1_Ani.py
@@ -0,0 +1,31 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+numero_1 = 5
+#escribir el nombre de la variable solo no se leer en la terminal. Consultar porque
+numero_1
+print(numero_1)
+numero_2 = 7
+numero_2
+print(numero_2)
+
+# Realizar la suma de las dos variables
+# numero_1 y numero_2
+# Almacenar el valor de la suma en una variable
+# ej:
+
+operacion_suma = numero_1+numero_2
+
+# Imprimir en pantalla el resultado de la suma
+print(operacion_suma)
+
+# Repita el procedimiento para realizar la resta
+operacion_resta = numero_1-numero_2
+print(operacion_resta )
---
+++ ejercicios_practica/Clase_2_ejercicio_2_Ani.py
@@ -0,0 +1,63 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica y consola
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+print('Ingrese por consola el primer número entero a operar:')
+numero_1 = int(input())
+print('Ingrese por consola el segundo número entero a operar:')
+numero_2 = int(input())
+# Alumno: Imprima en pantalla los dos números enteros solicitados
+print('usted selecciono el numero:')
+print(numero_1)
+print('como primer numero. Gracias')
+
+print('usted selecciono el numero:')
+print(numero_2)
+print('como segundo numero. Gracias')
+
+
+# Alumno: Calcule la suma, resta, división y multiplicación de los números ingresados
+# numero_1, numero_2
+# Imprima en pantalla todos los resultados con el siguiente formato de ejemplo:
+# El resultado de sumar 4 y 2 es 6
+# NOTA: No coloque usted los nùmeros y resultados, use las variables
+
+Suma=numero_1+numero_2
+print('El resultado de sumar:')
+print(numero_1)
+print('+')
+print(numero_2)
+print('es:')
+print(Suma)
+
+Resta=numero_1+numero_2
+print('El resultado de restar:')
+print(numero_1)
+print('-')
+print(numero_2)
+print('es:')
+print(Resta)
+
+División=numero_1/numero_2
+print('El resultado de dividir:')
+print(numero_1)
+print('/')
+print(numero_2)
+print('es:')
+print(División)
+Multiplicación=numero_1*numero_2
+print('El resultado de multiplicar:')
+print(numero_1)
+print('*')
+print(numero_2)
+print('es:')
+print(Multiplicación)
